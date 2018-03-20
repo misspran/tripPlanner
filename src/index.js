7bf9e7d6ec84011bf2a148102a23e7af44423713
Vi Tran <vipran@gmail.com>
2018-03-20 11:29:44 -0500

    added stuff

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Hello from JavaScript");
+const mapboxgl = require("mapbox-gl");
+const buildMarker = require("./marker");
+
+const marker = buildMarker("activities", [-87.6354, 41.8885]); // or [-87.6354, 41.8885]
+marker.addTo(map);
+
+mapboxgl.accessToken = "pk.eyJ1IjoibWlzc3ByYW4iLCJhIjoiY2plenR3YzU4MGUycDJxcW9ndzBqdGxuMCJ9.f3gj8qrycuC8H9aZDc4K4g";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-87.6354, 41.8885], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+
+
+//new mapboxgl.Marker(markerDomEl).setLngLat([-87.6354, 41.8885] ).addTo(map); // [-87.6354, 41.8885] for Chicago
---
+++ src/marker.js
@@ -0,0 +1,30 @@
+const mapbox = require("mapbox-gl");
+
+const iconURLs = {
+    hotels: "http://i.imgur.com/D9574Cu.png",
+    restaurants: "http://i.imgur.com/cqR6pUI.png",
+    activities: "http://i.imgur.com/WbMOfMl.png"
+};
+
+const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+
+const buildMarker = function (type, coords) {
+	if(type==="hotels"){
+		markerDomEl.style.backgroundImage = `url(${iconURLs.hotels})`
+	}else if(type === 'restaurants'){
+		markerDomEl.style.backgroundImage = `url(${iconURLs.restaurants})`
+
+	}else if(type==='activities'){
+		markerDomEl.style.backgroundImage = `url(${iconURLs.activities})`
+	}
+    
+};
+
+
+
+
+
+
+module.exports = buildMarker;
